8a6e3a13c32093b060f81682c9d528fc9ff70ca3
bobbyhadz <opmftw@gmail.com>
2024-04-09 09:36:13 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Remove the first N characters from a String in Python
+
+string = 'bobbyhadz.com'
+
+# ✅ Remove the first 2 characters from a string
+new_string = string[2:]
+print(new_string)  # 👉️ bbyhadz.com
+
+# ✅ Remove the first 3 characters from a string
+new_string = string[3:]
+print(new_string)  # 👉️ byhadz.com
+
+# ✅ Remove the first 4 characters from a string
+new_string = string[4:]
+print(new_string)  # 👉️ yhadz.com
